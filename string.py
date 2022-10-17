81e1171e9b3186af2cc746723b2385c9e72cefe9
Erik Karenovich <78443570+thekarenovich@users.noreply.github.com>
2022-10-18 02:04:13 +0300

    Create string.py

---
+++ string.py
@@ -0,0 +1,23 @@
+String = "Welcome to GeeksForGeeks"
+print("Creating String: ")
+print(String)
+      
+# Printing First character
+print("\nFirst character of String is: ")
+print(String[0])
+      
+# Printing Last character
+print("\nLast character of String is: ")
+print(String[-1])
+
+#______________________________________
+'''
+Creating String: 
+Welcome to GeeksForGeeks
+
+First character of String is: 
+W
+
+Last character of String is: 
+s
+'''
