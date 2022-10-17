fc42ecf52365051952f1c62e4fea60e40bbc0911
Erik Karenovich <78443570+thekarenovich@users.noreply.github.com>
2022-10-18 02:00:10 +0300

    Create list_or_array.py

---
+++ list_or_array.py
@@ -0,0 +1,44 @@
+# Creating a List with
+# the use of multiple values
+List = ["Geeks", "For", "Geeks"]
+print("\nList containing multiple values: ")
+print(List)
+  
+# Creating a Multi-Dimensional List
+# (By Nesting a list inside a List)
+List2 = [['Geeks', 'For'], ['Geeks']]
+print("\nMulti-Dimensional List: ")
+print(List2)
+  
+# accessing a element from the
+# list using index number
+print("\nAccessing element from the list")
+print(List[0])
+print(List[2])
+  
+# accessing a element using
+# negative indexing
+print("\nAccessing element using negative indexing")
+      
+# print the last element of list
+print(List[-1])
+      
+# print the third last element of list
+print(List[-3])
+
+# ____________________________________
+'''
+List containing multiple values: 
+['Geeks', 'For', 'Geeks']
+
+Multi-Dimensional List: 
+[['Geeks', 'For'], ['Geeks']]
+
+Accessing element from the list
+Geeks
+Geeks
+
+Accessing element using negative indexing
+Geeks
+Geeks
+'''
