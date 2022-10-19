c7097167156578146fb4d5402af61c3daa6aa367
Erik Karenovich <78443570+thekarenovich@users.noreply.github.com>
2022-10-19 04:05:46 +0300

    Update 1.py

--- graph/1.py
+++ graph/1.py
@@ -1,6 +1,6 @@
 # A simple representation of graph using Adjacency Matrix
 class Graph:
-    def __init__(self, numvertex):
+    def __init__(self, numvertex): 
         self.adjMatrix = [[-1]*numvertex for x in range(numvertex)]
         self.numvertex = numvertex
         self.vertices = {}
