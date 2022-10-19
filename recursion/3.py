5d6245b1731a37449be1ee5bd670283096c9688a
Erik Karenovich <78443570+thekarenovich@users.noreply.github.com>
2022-10-20 01:15:26 +0300

    Create 3.py

---
+++ recursion/3.py
@@ -0,0 +1,15 @@
+# The function fun() calculates and returns ((1 + 2 … + x-1 + x) +y), which is x(x+1)/2 + y. 
+# For example, if x is 5 and y is 2, then fun should return 15 + 2 = 17.
+
+def fun1(x, y) :
+ 
+    if (x == 0) :
+        return y
+    else :
+        return fun1(x - 1, x + y)
+
+print(fun1(5, 2))     
+#________________
+'''
+17
+'''
