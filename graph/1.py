3e489e19d943beb550cee64b87fc1104fde67787
Erik Karenovich <78443570+thekarenovich@users.noreply.github.com>
2022-10-19 04:06:30 +0300

    Update 1.py

--- graph/1.py
+++ graph/1.py
@@ -3,7 +3,7 @@ class Graph:
     def __init__(self, numvertex): 
         self.adjMatrix = [[-1]*numvertex for x in range(numvertex)]
         self.numvertex = numvertex
-        self.vertices = {} 
+        self.vertices = {}  
         self.verticeslist =[0]*numvertex 
   
     def set_vertex(self, vtx, id): 
